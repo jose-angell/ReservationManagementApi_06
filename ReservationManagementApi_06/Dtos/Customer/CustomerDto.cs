9e108b2c19907aec49bac64203719ebc953410cf
jose Angel <josegallardo.cor@gmail.com>
2026-08-11 22:38:39 -0600

    feat: dtos para customer

---
+++ ReservationManagementApi_06/Dtos/Customer/CustomerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ReservationManagementApi_06.Dtos.Customer
+{
+    public class CustomerDto
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+
+    }
+}
